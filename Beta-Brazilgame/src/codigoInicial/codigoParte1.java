de00bcb483d74ec1cc1023981ab8e03dab54d222
WIN10 <WIN10@DESKTOP-8S87C41>
2021-04-09 16:17:39 -0300

    upload

--- Beta-Brazilgame/src/codigoInicial/codigoParte1.java
+++ Beta-Brazilgame/src/codigoInicial/codigoParte1.java
@@ -1,22 +1,21 @@
 package codigoInicial;
 
-import jplay.Window;
-import jplay.GameImage;
-import jplay.URL;
+import javax.swing.JFrame;
 
-public class codigoParte1 {
-
-	public static void main(String[] args) {
-		
-		Window janela = new Window(1366, 768);
-		GameImage plano = new GameImage(URL.sprite("Fundo_cinza.jpg"));
-		
-			while(true) {
-				plano.draw();
-				janela.update();
-			}
-	}
+public class codigoParte1 extends JFrame {
+	
+	public codigoParte1() {
+		setTitle("Brazil Game - Beta");
+		setSize(1366,768);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		this.setResizable(true);
+		setVisible(true);
 		
 	}
-
+	
+	public static void main (String []args) {
+		new codigoParte1();
+	}
+}
 
